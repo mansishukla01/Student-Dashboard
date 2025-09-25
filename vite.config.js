939ec470b7cb6c191a05620448f7040a735511d7
mansishukla01 <shuklamansi493@gmail.com>
2025-09-25 11:50:51 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  base: './',  // important for correct relative paths in dist
+  build: {
+    outDir: 'dist'  // matches vercel.json
+  }
 });
